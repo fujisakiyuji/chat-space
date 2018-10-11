3cc09954957694d7c664185333efa3bb2ac097cf
fujisaki yuji <tairagantz@gmail.com>
2018-10-11 15:09:17 +0900

    revision

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
   resources :users, only: [:index, :edit, :update]
-  collection do
-    get "search"
-  end
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
