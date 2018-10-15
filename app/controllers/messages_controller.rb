252185f16576832df5a7aa1263c0c760ea4677ef
fujisaki yuji <tairagantz@gmail.com>
2018-10-15 19:13:29 +0900

    Implementation of automatic update function
    自動更新機能を実装した。

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,15 +3,18 @@ class MessagesController < ApplicationController
 
   def index
     @message = Message.new
-    @messages = @group.messages.includes(:user)
+    @messages = @group.messages.order(created_at: :ASC).includes(:user)
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   def create
     @message = @group.messages.new(message_params)
     if @message.save
-      # redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
       respond_to do |format|
-        format.html
+        format.html{ redirect_to group_messages_path(params[:group_id])  }
         format.json
       end
     else
